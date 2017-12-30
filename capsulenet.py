9eebb83b99170c562590a6b08edce48516fe7b59
Koki Wataoka <contact@wataoka.com>
2017-12-30 11:42:05 +0900

    コメントの日本語訳

--- capsulenet.py
+++ capsulenet.py
@@ -1,19 +1,17 @@
 """
-Keras implementation of CapsNet in Hinton's paper Dynamic Routing Between Capsules.
-The current version maybe only works for TensorFlow backend. Actually it will be straightforward to re-write to TF code.
-Adopting to other backends should be easy, but I have not tested this. 
+Hinton氏の論文「Dynamic Routing Between Capsules」のカプセルネットワークをKerasで実装した.
+おそらくバックエンドでTensorFlowでのみ動く.
+試してはいないが, 別のバックエンドに適応させることは簡単である.
 
-Usage:
-       python capsulenet.py
-       python capsulenet.py --epochs 50
-       python capsulenet.py --epochs 50 --routings 3
-       ... ...
-       
-Result:
-    Validation accuracy > 99.5% after 20 epochs. Converge to 99.66% after 50 epochs.
-    About 110 seconds per epoch on a single GTX1070 GPU card
-    
-Author: Xifeng Guo, E-mail: `guoxifeng1990@163.com`, Github: `https://github.com/XifengGuo/CapsNet-Keras`
+使い方:
+    python capsulenet.py
+    python capsulenet.py --epochs 50
+    python capsulenet.py --epochs 50 --routings 3
+
+結果:
+    validation accuracy = 99.50% (epochs=20)
+    validation accuracy = 99.66% (epochs=50)
+    ※single GTX1070を使用
 """
 
 import numpy as np
@@ -30,46 +28,52 @@ K.set_image_data_format('channels_last')
 
 def CapsNet(input_shape, n_class, routings):
     """
-    A Capsule Network on MNIST.
-    :param input_shape: data shape, 3d, [width, height, channels]
-    :param n_class: number of classes
-    :param routings: number of routing iterations
-    :return: Two Keras Models, the first one used for training, and the second one for evaluation.
-            `eval_model` can also be used for training.
-    """
-    x = layers.Input(shape=input_shape)
+    MNISTに関するカプセルネットワーク
 
-    # Layer 1: Just a conventional Conv2D layer
+    :param input_shape: 入力データのshape(3次元で[width, height, channels]という形)
+    :param n_class: クラスの数
+    :param routings: routingを行う回数
+
+    :return 2つのmodel (1つ目:学習用モデル, 2つ目:評価用モデル)
+            `eval_model`というモデルも学習用としてしようすることもできる.
+    """
+    print(input_shape)
+    x = layers.Input(shape=input_shape)
+    print(x)
+    quit()
+
+
+    # 1層目: ただの2次元畳み込み層
     conv1 = layers.Conv2D(filters=256, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
 
-    # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
+    # 2層目: 活性化関数にsquash関数を用いた2次元畳み込み層で,[None ,num_capsule, dim_capsule]という形に変換する
     primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=9, strides=2, padding='valid')
 
-    # Layer 3: Capsule layer. Routing algorithm works here.
+    # 3層目: カプセル層 (routingアルゴリズムはここで行っている)
     digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16, routings=routings,
                              name='digitcaps')(primarycaps)
 
-    # Layer 4: This is an auxiliary layer to replace each capsule with its length. Just to match the true label's shape.
-    # If using tensorflow, this will not be necessary. :)
+    # 4層目: ここはカプセルを"長さ"に変形するための補助レイヤーで, 教師データの形に合わせている.
+    # tensorflowを使用している場合, ここは必要ありません.
     out_caps = Length(name='capsnet')(digitcaps)
 
-    # Decoder network.
+    # Decoderネットワーク
     y = layers.Input(shape=(n_class,))
-    masked_by_y = Mask()([digitcaps, y])  # The true label is used to mask the output of capsule layer. For training
-    masked = Mask()(digitcaps)  # Mask using the capsule with maximal length. For prediction
+    masked_by_y = Mask()([digitcaps, y])  # 正解のラベルはよく学習のためにカプセル層の出力を隠す
+    masked = Mask()(digitcaps)  # マスクは予測のために長さが最大値のカプセルを使用する
 
-    # Shared Decoder model in training and prediction
+    # このDecoderモデルは学習時にも予測時にも使われる.
     decoder = models.Sequential(name='decoder')
     decoder.add(layers.Dense(512, activation='relu', input_dim=16*n_class))
     decoder.add(layers.Dense(1024, activation='relu'))
     decoder.add(layers.Dense(np.prod(input_shape), activation='sigmoid'))
     decoder.add(layers.Reshape(target_shape=input_shape, name='out_recon'))
 
-    # Models for training and evaluation (prediction)
+    # 学習モデルと評価モデル
     train_model = models.Model([x, y], [out_caps, decoder(masked_by_y)])
     eval_model = models.Model(x, [out_caps, decoder(masked)])
 
-    # manipulate model
+    # モデルにノイズを加える
     noise = layers.Input(shape=(n_class, 16))
     noised_digitcaps = layers.Add()([digitcaps, noise])
     masked_noised_y = Mask()([noised_digitcaps, y])
@@ -79,10 +83,11 @@ def CapsNet(input_shape, n_class, routings):
 
 def margin_loss(y_true, y_pred):
     """
-    Margin loss for Eq.(4). When y_true[i, :] contains not just one `1`, this loss should work too. Not test it.
+    論文中の方程式(4)にあたる.
+    y_trueはone_hot形式です。
     :param y_true: [None, n_classes]
-    :param y_pred: [None, num_capsule]
-    :return: a scalar loss value.
+    :param y_pred: [Noen, num_capsule]
+    :return: loss値(スカラー)
     """
     L = y_true * K.square(K.maximum(0., 0.9 - y_pred)) + \
         0.5 * (1 - y_true) * K.square(K.maximum(0., y_pred - 0.1))
@@ -92,16 +97,16 @@ def margin_loss(y_true, y_pred):
 
 def train(model, data, args):
     """
-    Training a CapsuleNet
-    :param model: the CapsuleNet model
-    :param data: a tuple containing training and testing data, like `((x_train, y_train), (x_test, y_test))`
+    カプセルネットの学習
+    :param model: カプセルネットのモデル
+    :param data: trainデータもtestデータも含んだタプルデータ, 右のような形式 `((x_train, y_train), (x_test, y_test))`
     :param args: arguments
-    :return: The trained model
+    :return: 学習ずみモデル
     """
-    # unpacking the data
+    # データ切り分け
     (x_train, y_train), (x_test, y_test) = data
 
-    # callbacks
+    # コールバック
     log = callbacks.CSVLogger(args.save_dir + '/log.csv')
     tb = callbacks.TensorBoard(log_dir=args.save_dir + '/tensorboard-logs',
                                batch_size=args.batch_size, histogram_freq=int(args.debug))
@@ -109,34 +114,35 @@ def train(model, data, args):
                                            save_best_only=True, save_weights_only=True, verbose=1)
     lr_decay = callbacks.LearningRateScheduler(schedule=lambda epoch: args.lr * (args.lr_decay ** epoch))
 
-    # compile the model
+    # モデルをコンパイル
     model.compile(optimizer=optimizers.Adam(lr=args.lr),
                   loss=[margin_loss, 'mse'],
                   loss_weights=[1., args.lam_recon],
                   metrics={'capsnet': 'accuracy'})
 
     """
-    # Training without data augmentation:
-    model.fit([x_train, y_train], [y_train, x_train], batch_size=args.batch_size, epochs=args.epochs,
-              validation_data=[[x_test, y_test], [y_test, x_test]], callbacks=[log, tb, checkpoint, lr_decay])
+    データ増強無しの学習をする場合:
+    model.fit([x_train, y_train], [y_train, x_train], bacth_size=args.batch_size, epochs=args.epochs,
+               validation_data=[[x_test, y_test], [y_test, x_test]], callbacks=[log, tb, checkpoint, lr_decay])
+    としてください
     """
 
-    # Begin: Training with data augmentation ---------------------------------------------------------------------#
+    # 開始 : データ増強ありの学習 ---------------------------------------------------------------------#
     def train_generator(x, y, batch_size, shift_fraction=0.):
         train_datagen = ImageDataGenerator(width_shift_range=shift_fraction,
-                                           height_shift_range=shift_fraction)  # shift up to 2 pixel for MNIST
+                                           height_shift_range=shift_fraction)  # MNISTの画像を2ピクセルをずらす
         generator = train_datagen.flow(x, y, batch_size=batch_size)
         while 1:
             x_batch, y_batch = generator.next()
             yield ([x_batch, y_batch], [y_batch, x_batch])
 
-    # Training with data augmentation. If shift_fraction=0., also no augmentation.
+    # shift_fraction=0とすれば, データ増強無しの学習.
     model.fit_generator(generator=train_generator(x_train, y_train, args.batch_size, args.shift_fraction),
                         steps_per_epoch=int(y_train.shape[0] / args.batch_size),
                         epochs=args.epochs,
                         validation_data=[[x_test, y_test], [y_test, x_test]],
                         callbacks=[log, tb, checkpoint, lr_decay])
-    # End: Training with data augmentation -----------------------------------------------------------------------#
+    # 終了 : データ増強無しの学習 -----------------------------------------------------------------------#
 
     model.save_weights(args.save_dir + '/trained_model.h5')
     print('Trained model saved to \'%s/trained_model.h5\'' % args.save_dir)
@@ -189,7 +195,7 @@ def manipulate_latent(model, data, args):
 
 
 def load_mnist():
-    # the data, shuffled and split between train and test sets
+    # 学習用データとテストデータを切り分け, シャッフルしたデータ
     from keras.datasets import mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
@@ -206,7 +212,7 @@ if __name__ == "__main__":
     from keras.preprocessing.image import ImageDataGenerator
     from keras import callbacks
 
-    # setting the hyper parameters
+    # ハイパーパラメータの設定
     parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
     parser.add_argument('--epochs', default=50, type=int)
     parser.add_argument('--batch_size', default=100, type=int)
@@ -235,21 +241,21 @@ if __name__ == "__main__":
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
 
-    # load data
+    # データの読み込み
     (x_train, y_train), (x_test, y_test) = load_mnist()
 
-    # define model
+    # モデル定義
     model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
                                                   n_class=len(np.unique(np.argmax(y_train, 1))),
                                                   routings=args.routings)
     model.summary()
 
-    # train or test
-    if args.weights is not None:  # init the model weights with provided one
+    # 学習orテスト
+    if args.weights is not None:  # 重みの初期化
         model.load_weights(args.weights)
     if not args.testing:
         train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
-    else:  # as long as weights are given, will run testing
+    else:  # 重みが与えられたなら, 開始.
         if args.weights is None:
             print('No weights are provided. Will test using random initialized weights.')
         manipulate_latent(manipulate_model, (x_test, y_test), args)
